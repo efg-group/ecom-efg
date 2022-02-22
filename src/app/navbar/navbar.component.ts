781b75c2bf46fe90fc88993fef38a25b74a86679
mehdad-hussain <hussain.mehdad1@gmail.com>
2022-02-22 12:10:54 +0600

    flex

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -118,8 +118,8 @@ export class NavbarComponent  {
     }
   }
   breakpoints = {
-    360: { slidesPerView: 1.5, spaceBetween: 20 },
-    640: { slidesPerView: 2.5, spaceBetween: 20 },
+    360: { slidesPerView: 2, spaceBetween: 20 },
+    640: { slidesPerView: 2, spaceBetween: 20 },
     768: { slidesPerView: 3.5, spaceBetween: 10 },
     1024: { slidesPerView: 4.5, spaceBetween: 20 }
   };
